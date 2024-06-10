0e4947eb017c546a6a8c4dd7034164d0a2f112b7
maameyaa <evelynyaabaidoo@gmail.com>
2024-06-10 10:23:14 +0000

    first commit

---
+++ js/index.js
@@ -0,0 +1,11 @@
+const btn = document.getElementById('btn-btn')
+
+
+btn.onclick = function () {
+    alert('More Services Coming Soon')
+}
+
+const sen = document.getElementById('ser') 
+sen.onclick = function () {
+    alert('Amazing Services')
+}
